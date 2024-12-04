9a17340d1c85ffe569649859efadd768223f1904
balivishalsingh <sbalivishal@gmail.com>
2024-12-04 11:49:35 +0530

    Object array

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-const student = 
-{
-    firstName:"Aman",
-    lastName:"singh",
-    dob:"20th april",
-    pinCode:254983  
-}
-console.log(student["dob"]);
+//   object array
+ const student = [
+    {
+        name:"Aman singh",
+        rollno:11,
+        marks:95
+    },
+    {
+        name:"Anish",
+        rollno:15,
+        marks:85
+    },
+    {
+        name:"Anuj",
+        rollno:19,
+        marks:83
+    },
+ ]
+ 
+ console.log(student[0].name);
