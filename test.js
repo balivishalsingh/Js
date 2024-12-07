cd0d371ad0ce6c001e650a301c076dc3f4e810ca
balivishalsingh <sbalivishal@gmail.com>
2024-12-07 12:04:45 +0530

    Switch

--- test.js
+++ test.js
@@ -1,23 +1,10 @@
-class student
+switch(2)
 {
-    constructor(firstName,lastName)
-    {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+    case 1:
+    console.log("switch");
+    case 2:
+        console.log("Swith");
+     case 3:
+        console.log("Stch");
 
-    displayFullname()
-{
-    return (this.firstName + " " + this.lastName);
 }
-}
-
-
-const m1 = new student("Akash","Singh");
-const m2 = new student("Akansha","Raput");
-const name1 = m1.displayFullname();
-const name2 = m2.displayFullname();
-console.log(name1);
-console.log(name2);
-
-
